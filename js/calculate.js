d1857cb2208f5d5de5eb70c60806cc00efa5943d
lanxiaozhi <kouz15@mails.tsinghua.edu.cn>
2018-03-10 16:21:31 +0800

    modify

--- js/calculate.js
+++ js/calculate.js
@@ -1,70 +1,63 @@
 var totalPile = 0;
-var square = new Array();
-var length = 7296;
-for(var i=0;i<length;i++){
-  square[i] = new Object();
-  square[i].prob = new Array();
+var square = new Array(); //格点信息
+var length = 7296;    //格点数
+var grid = new Array();   //网格坐标
+var gridClip = new Array();   //有效网格坐标
+var district = new Array();   //行政区划信息
+var totalQuickRate = new Array();   //快充功效函数
+var totalSlowRate = new Array();    //慢充功效函数
+var pile = new Array();   //充电桩分布
+var quickPile = new Array();    //快充分布
+var slowPile = new Array();   //慢充分布
+var event_2 = new Array(161007);    //0.02对应事件数据
+var event_5 = new Array(403423);    //0.05对应事件数据
+var event_10 = new Array(806551);   //0.1对应事件数据
+
+function init(){    //初始化
+  for(var i=0;i<length;i++){
+    square[i] = new Object();
+    square[i].prob = new Array();
+  }
+  for(var i=0;i<19272;i++){
+    grid[i] = new Object();
+  }
+  for(var i=0;i<7296;i++){
+    gridClip[i] = new Object();
+  }
+  for(var i=0;i<length;i++){
+    district[i] = new Object();
+  }
+  for(var i=0;i<square.length;i++){
+    totalQuickRate[i]=0;
+    totalSlowRate[i]=0;
+  }
+  for(var i=0;i<square.length;i++) {
+    pile[i] = 0;
+    quickPile[i] = 0;
+    slowPile[i] = 0;
+  }
+  for(var i=0;i<event_2.length;i++){
+    event_2[i] = new Object();
+  }
+  for(var i=0;i<event_5.length;i++){
+    event_5[i] = new Object();
+  }
+  for(var i=0;i<event_10.length;i++){
+    event_10[i] = new Object();
+  }
+  for(var i=0;i<10;i++){
+    disQuickPile[i] = 0;
+    disSlowPile[i] = 0;
+    disQuickRate[i] = 0;
+    disSlowRate[i] = 0;
+  }
+  for(var i=0;i<10;i++){
+    count1[i] = 0;
+    count2[i] = 0;
+  }
 }
 
-var grid = new Array();
-for(var i=0;i<19272;i++){
-  grid[i] = new Object();
-}
-
-var gridClip = new Array();
-for(var i=0;i<7296;i++){
-  gridClip[i] = new Object();
-}
-
-var district = new Array();
-for(var i=0;i<length;i++){
-  district[i] = new Object();
-}
-
-var time = new Array();
-for(var i=0;i<24;i++){
-  time[i] = new Array(4);
-}
-
-var totalQuickRate = new Array();
-for(var i=0;i<square.length;i++){
-  totalQuickRate[i]=0;
-}
-
-var totalSlowRate = new Array();
-for(var i=0;i<square.length;i++){
-  totalSlowRate[i]=0;
-}
-
-var pile = new Array();
-for(var i=0;i<square.length;i++){
-  pile[i] = 0;
-}
-
-var quickPile = new Array();
-for(var i=0;i<square.length;i++){
-  quickPile[i] = 0;
-}
-
-var slowPile = new Array();
-for(var i=0;i<square.length;i++){
-  slowPile[i] = 0;
-}
-
-var event_2 = new Array(161007);
-for(var i=0;i<event_2.length;i++){
-  event_2[i] = new Object();
-}
-var event_5 = new Array(403423);
-for(var i=0;i<event_5.length;i++){
-  event_5[i] = new Object();
-}
-var event_10 = new Array(806551);
-for(var i=0;i<event_10.length;i++){
-  event_10[i] = new Object();
-}
-
-function calcQuickRate(){
+function calcQuickRate(){ //计算总和
   var result = 0;
   for(var i=0;i<totalQuickRate.length;i++){
     result += Number(totalQuickRate[i]);
@@ -72,7 +65,7 @@ function calcQuickRate(){
   return result;
 }
 
-function calcSlowRate(){
+function calcSlowRate(){    //计算总和
   var result = 0;
   for(var i=0;i<totalSlowRate.length;i++){
     result += Number(totalSlowRate[i]);
@@ -80,16 +73,10 @@ function calcSlowRate(){
   return result;
 }
 
-var disQuickPile = new Array();
-var disSlowPile = new Array();
-var disQuickRate = new Array();
-var disSlowRate = new Array();
-for(var i=0;i<10;i++){
-  disQuickPile[i] = 0;
-  disSlowPile[i] = 0;
-  disQuickRate[i] = 0;
-  disSlowRate[i] = 0;
-}
+var disQuickPile = new Array();   //区划快充数量
+var disSlowPile = new Array();    //区划慢充数量
+var disQuickRate = new Array();   //区划快充功效函数
+var disSlowRate = new Array();    //区划慢充功效函数
 
 var currentQuickRate = 1000000;
 var currentSlowRate = 1000000;
@@ -97,11 +84,8 @@ var totalQuickMissRate = 0,totalQuickFillRate = 0,totalSlowMissRate = 0, totalSl
 var quickCount = 0, slowCount = 0;
 var count1 = new Array();
 var count2 = new Array();
-for(var i=0;i<10;i++){
-  count1[i] = 0;
-  count2[i] = 0;
-}
-function calcPile() {
+
+function calcPile() {   //计算主函数
   calcEvent();
   while(1){
     currentQuickRate = calcQuickRate();
@@ -146,13 +130,8 @@ function calcPile() {
   }
 }
 
-function swap(a,b){
-  var temp = a;
-  a = b;
-  b = temp;
-}
 var quickOrder = new Array();
-function adjustQuickPile(){
+function adjustQuickPile(){   //快充调整分布
   for(var i=0;i<totalQuickRate.length;i++){
     quickOrder[i] = i;
   }
@@ -180,7 +159,7 @@ function adjustQuickPile(){
 }
 
 var slowOrder = new Array();
-function adjustSlowPile(){
+function adjustSlowPile(){    //慢充调整分布
   for(var i=0;i<totalSlowRate.length;i++){
     slowOrder[i] = i;
   }
@@ -209,14 +188,14 @@ function adjustSlowPile(){
 
 var quickCharge = new Array();
 var slowCharge = new Array();
-var quickChargingTime = new Array();
-var slowChargingTime = new Array();
-var quickMiss = new Array();
-var slowMiss = new Array();
-var quickChargingEvent = new Array();
-var slowChargingEvent = new Array();
+var quickChargingTime = new Array();    //快充总时间
+var slowChargingTime = new Array();    //慢充总时间
+var quickMiss = new Array();    //慢充不满足数
+var slowMiss = new Array();   //慢充不满足数
+var quickChargingEvent = new Array();   //快充总事件
+var slowChargingEvent = new Array();    //慢充总事件
 
-function judgeCharging(id,startTime,type){
+function judgeCharging(id,startTime,type){    //记录充电时长
   if(type === 0){
     quickCharge[id][startTime].charging += 1;
   }
@@ -225,7 +204,7 @@ function judgeCharging(id,startTime,type){
   }
 }
 
-function judgePile(id,startTime,eventDur) {
+function judgePile(id,startTime,eventDur) {   //快慢充使用区分
   if(eventDur <= 4) {
     quickChargingEvent[id] += 1;
 
@@ -262,7 +241,7 @@ function judgePile(id,startTime,eventDur) {
   }
 }
 
-function calcEvent() {
+function calcEvent() {    //计算事件满足度
   for(var i=0;i<square.length;i++){
     quickChargingTime[i] = 0;
     slowChargingTime[i] = 0;
@@ -345,39 +324,6 @@ function calcEvent() {
       totalSlowRate[i] = 0;
     }
   }
-  /*for(var i=0;i<square.length;i++){
-    var squareEvent = event[i];
-    /*var totalChargingTime = 0;
-    var totalMiss = 0;
-    var totalChargingEvent = 0;
-    for(var j=0;j<24;j++){
-      event[j] = new Object();
-      event[j].toCharge = square[i].prob[j];
-      event[j].newCharge = 0;
-      event[j].charging = 0;
-    }
-    for(var j=0;j<24;j++){
-      totalChargingEvent[i] += squareEvent[j].toCharge;
-      if(pile[i] - squareEvent[j].charging >= squareEvent[j].toCharge){
-        squareEvent[j].charging += squareEvent[j].toCharge;
-        //squareEvent[j].newCharge = squareEvent[j].toCharge;
-        totalChargingTime[i] += squareEvent[j].charging;
-      }
-      else{
-        var newCharge = pile[i] - squareEvent[j].charging;
-        squareEvent[j].charging = pile[i];
-        totalMiss[i] += squareEvent[j].toCharge - newCharge;
-      }
-    }
-    if(pile[i] > 0 && totalChargingEvent[i] > 0) {
-      var missRate = totalMiss[i] / totalChargingEvent[i];
-      var fillRate = totalChargingTime[i] / pile[i] / 24;
-      totalRate[i] = (0.5*fillRate + 0.5*(1-missRate)).toFixed(4);
-    }
-    else{
-      totalRate[i] = 0;
-    }
-  }*/
 }
 
 
